b07c707c4f91b184fced0f9f002de72dc38b23d0
PeterFarber <peterfarber@fullsail.edu>
2015-05-29 16:49:29 -0400

    Beta Milestone ( Annoy The Lubs )

--- BrainStorm/Assets/AnnoyChrisApp/Scripts/AnnoyChris.cs
+++ BrainStorm/Assets/AnnoyChrisApp/Scripts/AnnoyChris.cs
@@ -8,19 +8,25 @@ public class AnnoyChris : MonoBehaviour {
     public float Sanity;
     public float currentSanity;
     int State;
+    int clicks;
     public Sprite happy;
     public Sprite normal;
     public Sprite angry;
+    public Sprite fartface;
     private AudioSource audioSource;
     public AudioClip[] sounds;
     private Image sanityImage;
+    private Text Clicks;
+    private Animator animatorController;
 
 	void Start () {
-        Sanity = 30;
+        animatorController = GetComponent<Animator>();
+        Sanity = 10;
         currentSanity = Sanity;
         State = 0;
         audioSource = GetComponent<AudioSource>();
         sanityImage = GameObject.Find("Sanity").GetComponent<Image>();
+        Clicks = GameObject.Find("Clicks").GetComponent<Text>();
         sanityImage.color = Color.green;
 
 	}
@@ -35,40 +41,57 @@ public class AnnoyChris : MonoBehaviour {
             GetComponent<Image>().sprite = happy;
             sanityImage.color = Color.green;
         }
-        if (percentSanity <= 0.66f && percentSanity > 0.33f && State != 1)
+        else if (percentSanity <= 0.66f && percentSanity > 0.33f && State != 1)
         {
             State = 1;
             GetComponent<Image>().sprite = normal;
             sanityImage.color = Color.yellow;
         }
-        if (percentSanity <= 0.33f && percentSanity > 0.0f && State != 2)
+        else if (percentSanity <= 0.33f && percentSanity > 0.0f && State != 2)
         {
             State = 2;
             GetComponent<Image>().sprite = angry;
             sanityImage.color = Color.red;
         }
-
-
-        if (percentSanity < 1.0f)
+        else if( percentSanity <= 0.0f && State != 3)
         {
-            currentSanity += 2f * Time.deltaTime;
+            State = 3;
+            GetComponent<Image>().sprite = fartface;
+            sanityImage.color = Color.gray;
+            audioSource.clip = sounds[3];
+            audioSource.Play();
+            animatorController.SetBool("Spinning", true);
+            
         }
-        if (percentSanity <= 0.66f)
+
+        if (State != 3)
         {
-            currentSanity += 2f * Time.deltaTime;            
-        }
-        if (percentSanity <= 0.33f)
-        {
-            currentSanity += 2f * Time.deltaTime;
+            if (percentSanity < 1.0f)
+            {
+                currentSanity += 2f * Time.deltaTime;
+            }
+            if (percentSanity <= 0.66f)
+            {
+                currentSanity += 2f * Time.deltaTime;
+            }
+            if (percentSanity <= 0.33f)
+            {
+                currentSanity += 2f * Time.deltaTime;
+            }
         }
     }
 
     public void ClickEvent(){
-        currentSanity--;
-        if (Random.Range(0, 5) == 0)
+        if (State != 3)
         {
-            audioSource.clip = sounds[Random.Range(0, 3)];
-            audioSource.Play();
+            clicks++;
+            Clicks.text = clicks.ToString();
+            currentSanity--;
+            if (Random.Range(0, 5) == 0)
+            {
+                audioSource.clip = sounds[Random.Range(0, 3)];
+                audioSource.Play();
+            }
         }
     }
 }
